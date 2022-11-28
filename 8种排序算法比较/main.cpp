482f88a3908701b3682c1ee6d4c24023371ce856
afishspring <104251938+afishspring@users.noreply.github.com>
2022-11-29 00:46:26 +0800

    加入计数和计时
    添加注释；计时；
    计数为记录排序判断次数，
    可能有部分算法的计数不太正确

--- 8种排序算法比较/main.cpp
+++ 8种排序算法比较/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <Windows.h>
+#define setwNum 28
 #include "sort.h"
 using namespace std;
 int main()
@@ -18,37 +20,71 @@ int main()
 	cout << "请输入要产生的随机数的个数：";
 
 	int num, oper;
-	sort<int>x(15);
-	x.init(0,100);
-	x.BubbleSort();
-	cout << "bubble";
-	x.print();
+	cin >> num;
+	sort<int>x(num);
+	x.init(0,num);
+	while (true) {
+		cout << setiosflags(ios::left) << setw(setwNum) << "请选择排序算法：";
+		cin >> oper;
 
-	x.SeleteSort();
-	cout << "select";
-	x.print();
+		LARGE_INTEGER tick, begin, end;
+		QueryPerformanceFrequency(&tick); //获得计数器频率
+		QueryPerformanceCounter(&begin); //获得初始硬件计数器计数
 
-	x.InsertSort();
-	cout << "insert";
-	x.print();
-
-	x.ShellSort();
-	cout << "shell";
-	x.print();
-
-	x.QuickSort();
-	cout << "quick";
-	x.print();
-
-	x.HeapSort();
-	cout << "heap";
-	x.print();
-
-	x.MergeSort();
-	cout << "merge";
-	x.print();
-
-	x.RadixSort();
-	cout << "radix";
-	x.print();
+		switch (oper) {
+		case 1:
+			x.BubbleSort();
+			cout << setw(setwNum) << "冒泡排序交换次数：";
+			x.print();
+			cout << setw(setwNum) << "冒泡排序所用时间：";
+			break;
+		case 2:
+			x.SeleteSort();
+			cout << setw(setwNum) << "选择排序交换次数：";
+			x.print();
+			cout << setw(setwNum) << "选择排序所用时间：";
+			break;
+		case 3:
+			x.InsertSort();
+			cout << setw(setwNum) << "直接插入排序交换次数：";
+			x.print();
+			cout << setw(setwNum) << "直接插入排序所用时间：";
+			break;
+		case 4:
+			x.ShellSort();
+			cout << setw(setwNum) << "希尔插入排序交换次数：";
+			x.print();
+			cout << setw(setwNum) << "希尔插入排序所用时间：";
+			break;
+		case 5:
+			x.QuickSort();
+			cout << setw(setwNum) << "快速排序交换次数：";
+			x.print();
+			cout << setw(setwNum) << "快速排序所用时间：";
+			break;
+		case 6:
+			x.HeapSort();
+			cout << setw(setwNum) << "堆排序交换次数：";
+			x.print();
+			cout << setw(setwNum) << "堆排序所用时间：";
+			break;
+		case 7:
+			x.MergeSort();
+			cout << setw(setwNum) << "归并排序交换次数：";
+			x.print();
+			cout << setw(setwNum) << "归并排序所用时间：";
+			break;
+		case 8:
+			x.RadixSort();
+			cout << setw(setwNum) << "基数排序交换次数：";
+			x.print();
+			cout << setw(setwNum) << "基数排序所用时间：";
+			break;
+		case 9:
+			exit(1);
+			break;
+		}
+		QueryPerformanceCounter(&end);
+		cout << setiosflags(ios::fixed) << setprecision(6) << double(end.QuadPart - begin.QuadPart) / tick.QuadPart << "秒" << endl << endl;
+	}
 }
--- 8种排序算法比较/sort.h
+++ 8种排序算法比较/sort.h
@@ -9,34 +9,35 @@ class sort {
 public:
 	sort(int size = 100);
 	~sort();
-	void init(int minN, int maxN);
-	void BubbleSort();
-	void SeleteSort();
-	void InsertSort();
-	void ShellSort();
-	void QuickSort();
-	void HeapSort();
-	void MergeSort();
-	void RadixSort();
-	void print();
+	void init(int minN, int maxN);//生成minN~maxN的随机数
+	void BubbleSort();            //冒泡排序
+	void SeleteSort();            //选择排序
+	void InsertSort();            //直接插入排序
+	void ShellSort();             //希尔插入排序
+	void QuickSort();             //快速排序
+	void HeapSort();              //堆排序
+	void MergeSort();             //归并排序
+	void RadixSort();             //基数排序
+	void print();                 //打印交换次数或调试时观察排序结果
 private:
-	int ShellInsert(int add);
-	void qSort(int left, int right);
-	void maxHeap(int start,int end);
-	void merge(int low, int mid, int high);
-	void swap(int x, int y);
-	void copyData();
-	int maxDigit();
-	int sz, count = 0;
-	T* data;
-	T* res;
+	int ShellInsert(int add);                 //步长为add的希尔子排序，add为1即为直接插入排序
+	void qSort(int left, int right);          //快速排序
+	void maxHeap(int start,int end);          //大根堆
+	void merge(int low, int mid, int high);   //归并
+	void swap(int x, int y);                  //交换下标为x与y的值
+	void copyData();                          //复制源数据到res数组
+	int maxDigit();                           //用于基数排序的求数字最高位数的函数
+	int sz;                  //数据大小
+	long long count = 0;     //计数判断次数
+	T* data;  //存放源数据
+	T* res;   //表示排序过程和排序结果的数组
 };
 
 template<class T>
 inline sort<T>::sort(int size):sz(size)
 {
-	data = new T[sz];
-	res = new T[sz];
+	data = new T[sz]();
+	res = new T[sz]();
 }
 
 template<class T>
@@ -62,6 +63,7 @@ inline void sort<T>::BubbleSort()
 	count = 0;
 	for (int j = sz - 1; j >= 0; j--) {
 		for (int i = 0; i < j; i++) {
+			count++;
 			if (res[i] > res[i + 1]) {
 				swap(i, i + 1);
 			}
@@ -78,9 +80,9 @@ inline void sort<T>::SeleteSort()
 	for (int i = 0; i < sz-1; i++) {
 		minIndex = i;
 		for (int j = i + 1; j < sz; j++) {
+			count++;
 			if (res[minIndex] > res[j]) {
 				minIndex = j;
-				count++;
 			}
 		}
 		if (minIndex != i) {
@@ -93,7 +95,7 @@ template<class T>
 inline void sort<T>::InsertSort()
 {
 	copyData();
-	count = ShellInsert(1);
+	ShellInsert(1);
 }
 
 template<class T>
@@ -106,7 +108,7 @@ inline void sort<T>::ShellSort()
 	while (step > 1) {
 		step /= 2;
 		shellCount = ShellInsert(step);
-		count += shellCount;
+		//count += shellCount;
 	}
 }
 
@@ -143,7 +145,7 @@ inline void sort<T>::MergeSort()
 			mid = low + rangeLen - 1;
 			high = mid + rangeLen;
 			if (high > sz - 1)
-				high = sz - 1;
+				high = sz - 1; count++;
 			merge(low, mid, high);
 			low = high + 1;
 		}
@@ -186,17 +188,17 @@ inline void sort<T>::RadixSort()
 template<class T>
 inline void sort<T>::print()
 {
-	cout << endl << "before:" << endl;
-	for (int i = 0; i < sz; i++) {
-		if (i % 5 == 0)cout << endl;
-		cout << setiosflags(ios::left) << setw(10) << data[i];
-	}
-	cout << endl << "after:" << endl;
-	for (int i = 0; i < sz; i++) {
-		if (i % 5 == 0)cout << endl;
-		cout << setiosflags(ios::left) << setw(10) << res[i];
-	}
-	cout << endl << "swapTime:" << count << endl;
+	//cout << endl << "before:" << endl;
+	//for (int i = 0; i < sz; i++) {
+	//	if (i % 5 == 0)cout << endl;
+	//	cout << setiosflags(ios::left) << setw(10) << data[i];
+	//}
+	//cout << endl << "after:" << endl;
+	//for (int i = 0; i < sz; i++) {
+	//	if (i % 5 == 0)cout << endl;
+	//	cout << setiosflags(ios::left) << setw(10) << res[i];
+	//}
+	cout << count << endl;
 }
 
 template<class T>
@@ -208,6 +210,7 @@ inline int sort<T>::ShellInsert(int add)
 		nowNum = res[i];
 		iPos = i - add;
 		while (iPos >= 0 && res[iPos] > nowNum) {
+			count++;
 			res[iPos + add] = res[iPos];
 			iPos -= add;
 			k++;
@@ -221,21 +224,25 @@ inline int sort<T>::ShellInsert(int add)
 template<class T>
 inline void sort<T>::qSort(int left, int right)
 {
+	count++;
 	if (left >= right)
 		return;
 	T pivot = res[left];
 	int low = left, high = right;
 	while (low<high) {
 		while (res[high] >= pivot && low < high)
-			high--;
-		res[low] = res[high];
+			high--; count++;
+		if(low<high)
+			res[low++] = res[high]; count++;
 		while (res[low] <= pivot && low < high)
-			low++;
-		res[high] = res[low];
-		res[low] = pivot; count++;
-		qSort(left, low - 1);
-		qSort(low + 1, right);
+			low++; count++;
+		if(low<high)
+			res[high--] = res[low]; count++;
 	}
+
+	res[low] = pivot;
+	qSort(left, low - 1);
+	qSort(low + 1, right);
 }
 
 template<class T>
@@ -243,7 +250,10 @@ inline void sort<T>::maxHeap(int start,int end)
 {
 	int i = start, j = 2 * start + 1;
 	while (j <=end) {
-		if (j + 1 <= end && res[j] < res[j + 1])j++;
+		if (j + 1 <= end && res[j] < res[j + 1]) {
+			j++; count++;
+		}
+		count++;
 		if (res[i] > res[j])break;
 		else {
 			swap(i, j);
@@ -263,15 +273,16 @@ inline void sort<T>::merge(int low, int mid, int high)
 		exit(-1);
 	}
 	while (i <= mid && j <= high) {
+		count++;
 		if (res[i] <= res[j])
 			temp[k++] = res[i++];
 		else
 			temp[k++] = res[j++];
 	}
 	while (i <= mid)
-		temp[k++] = res[i++];
+		temp[k++] = res[i++]; count++;
 	while (j <= high)
-		temp[k++] = res[j++];
+		temp[k++] = res[j++]; count++;
 	for (int index = low, k = 0; index <= high; index++, k++)
 		res[index] = temp[k];
 	delete[]temp;
@@ -283,7 +294,6 @@ inline void sort<T>::swap(int x, int y)
 	T temp = res[x];
 	res[x] = res[y];
 	res[y] = temp;
-	count++;
 }
 
 template<class T>
