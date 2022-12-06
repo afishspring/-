b1aab91e04f7851f025bf63189bb01b4f1194ab6
afishspring <104251938+afishspring@users.noreply.github.com>
2022-12-06 21:57:44 +0800

    家谱终稿
    添加了打印家谱功能，添加成员不能重名的限制and说明文档

--- 家谱管理系统/main.cpp
+++ 家谱管理系统/main.cpp
@@ -1,38 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include "tree.h"
 using namespace std;
 
-void operation(Tree<string>&tr,char oper) {
-	string tempP, tempC;
+//封装对树进行增删改查的操作
+void operation(Tree<mystring>&tr,char oper) {
+	mystring tempP, tempC;
 	if (oper == 'A' || oper == 'a') {
 		cout << "请输入要建立家庭的人的姓名：";
 		cin >> tempP;
-		if (!tr.find(tempP)) {
+		if (!tr.find(tempP,true)) {
 			cerr << "家谱里没有成员" << tempP;
 		}
 		else {
 			int nChild;
-			cout << "请输入" << tempP << "的儿女个数：";
-			cin >> nChild;
+			while (true) {
+				cout << "请输入" << tempP << "的儿女个数：";
+				cin >> nChild;
+				if (cin.fail() || nChild <= 0) {
+					cin.clear();
+					cin.ignore(INT_MAX, '\n');
+					cerr << "数字输入非法！\n";
+					continue;
+				}
+				else
+					break;
+			}//输入儿女个数
 			cout << "请依次输入" << tempP << "的儿女的姓名：";
 			for (int i = 1; i <= nChild; i++) {
 				cin >> tempC;
-				if (!tr.insertChild(tempP, tempC))cerr << "error";
+				if (!tr.insertChild(tempP, tempC))//添加孩子的函数返回false值有父节点不存在和重名两种可能
+					cerr << "名字重复\n";//父节点存在性已在上面检验过了，所以这里只剩后一种返回为false
 			}
 			cout << tempP << "的第一代子孙是：";
 			tr.printCurrentChild();
 		}
 	}
-	else if (oper == 'B' || oper == 'b') {
+	else if (oper == 'B' || oper == 'b') {//该操作其实与A没多大区别
 		cout << "请输入要添加儿子（或女儿）的人的姓名：";
 		cin >> tempP;
-		if (!tr.find(tempP)) {
+		if (!tr.find(tempP,true)) {
 			cerr << "家谱里没有成员" << tempP;
 		}
 		else {
 			cout << "请输入" << tempP << "新添加的儿子（或女儿）的姓名：";
 			cin >> tempC;
-			if (!tr.insertChild(tempP, tempC))cerr << "error";
+			if (!tr.insertChild(tempP, tempC))
+				cerr << "名字重复\n";
 			cout << tempP << "的第一代子孙是：";
 			tr.printCurrentChild();
 		}
@@ -40,30 +54,31 @@ void operation(Tree<string>&tr,char oper) {
 	else if (oper == 'C' || oper == 'c') {
 		cout << "请输入要解散家庭的人的姓名：";
 		cin >> tempP;
-		if (!tr.find(tempP)) {
+		if (!tr.find(tempP,true)) {
 			cerr << "家谱里没有成员" << tempP;
 		}
 		else {
 			cout << "要解散家庭的是" << tempP << endl;
 			cout << tempP << "的第一代子孙是：";
-			tr.printCurrentChild();
+			tr.printCurrentChild();//输出在解散前，不然需要额外存姓名
 			tr.destroyFamily(tempP);
 		}
 	}
 	else {
 		cout << "请输入要更改姓名的人的当前姓名：";
 		cin >> tempP;
-		if (!tr.find(tempP)) {
+		if (!tr.find(tempP,true)) {
 			cerr << "家谱里没有成员" << tempP;
 		}
 		else {
 			cout << "请输入更改后的姓名：";
 			cin >> tempC;
-			tr.changeName(tempP, tempC);
-			cout << tempP << "已更改为" << tempC;
+			if (tr.changeName(tempP, tempC))
+				cout << tempP << "已更改为" << tempC;
 		}
 	}
 }
+
 
 int main() {
 	cout << "**                  家谱管理系统                    **" << endl;
@@ -73,15 +88,16 @@ int main() {
 	cout << "**                 B --- 添加家庭成员               **" << endl;
 	cout << "**                 C --- 解散局部家庭               **" << endl;
 	cout << "**                 D --- 更改家庭成员姓名           **" << endl;
-	cout << "**                 E --- 退出程序                   **" << endl;
+	cout << "**                 E --- 打印家谱                   **" << endl;
+	cout << "**                 F --- 退出程序                   **" << endl;
 	cout << "======================================================" << endl;
 	cout << "首先建立一个家谱！" << endl;
 
-	string name;
+	mystring name;
 	char oper;
 	cout << "请输入祖先的名字:";
 	cin >> name;
-	Tree<string>jp(name);
+	Tree<mystring>jp(name);
 	cout << "此家谱的祖先是：";
 	jp.printCurrent();
 
@@ -91,15 +107,15 @@ int main() {
 		cin.ignore(INT_MAX, '\n');
 		cin >> oper;
 		if (!(oper >= 'A' && oper <= 'E' || oper >= 'a' && oper <= 'e')) {
-			cerr << "输入的什么东西";
+			cerr << "输入非法操作符！";
 			continue;
 		}
-		if (oper == 'E' || oper == 'e') {
+		if (oper == 'F' || oper == 'f')
 			exit(-1);
-		}
-		else {
+		else if(oper == 'E' || oper == 'e')
+			jp.print();
+		else
 			operation(jp,oper);
-		}
 	}
 	return 0;
 }
--- 家谱管理系统/tree.h
+++ 家谱管理系统/tree.h
@@ -1,51 +1,55 @@
 #pragma once
 #include <iostream>
+#include <cstring>
+#define maxLength 10
 using namespace std;
 
 template<class T>
 struct TreeNode {
 	T data;
 	TreeNode<T>* firstChild, * nextSibling;
-	TreeNode(T val=0,TreeNode<T>*fc=NULL,TreeNode<T>*ns=NULL)
+	TreeNode(T val=0,TreeNode<T>*fc= nullptr,TreeNode<T>*ns= nullptr)
 		:data(val),firstChild(fc),nextSibling(ns){}
 };
 
 template<class T>
 class Tree {
 public:
-	Tree() { root = current = NULL; }
-	Tree(T ancestor);
-	~Tree() { RemoveSubTree(root); }
-	bool Root();
-	bool firstChild();
-	bool nextSibling();
-	bool parent();
-	bool find(T val);
-	bool isEmpty();
-	bool changeName(T nameBef, T nameNow);
-	bool insertChild(T father,T val);
-	bool insertSibling(T bother, T val);
-	bool destroyFamily(T father);
-	bool printCurrent();
-	void printCurrentChild();
+	Tree() { root = current = nullptr; }	//构造函数
+	Tree(T ancestor);						//含参构造函数
+	~Tree() { RemoveSubTree(root); }		//析构函数
+	bool Root();							//令当前节点为根节点
+	bool firstChild();						//转移到当前节点左子女节点
+	bool nextSibling();						//转移到当前节点的右兄弟节点
+	bool parent();							//转移到当前节点的父节点
+	bool find(T val,bool isTrans);			//转移到值为val的节点
+	bool isEmpty();							//判断树是否为空
+	bool changeName(T nameBef, T nameNow);	//改变节点的值
+	bool insertChild(T father,T val);		//添加左子女节点到father节点下
+	bool insertSibling(T bother, T val);	//添加右兄弟节点到bother节点下
+	bool destroyFamily(T father);			//解散father的所有后代，自身保留
+	bool printCurrent();					//打印当前节点
+	void printCurrentChild();				//打印当前节点的子女节点
+	void print() {PrintAsTree(root, 1);}	//打印树结构
 private:
-	TreeNode<T>* root, * current;
-	bool Find(TreeNode<T>* p, T val);
-	void RemoveSubTree(TreeNode<T>* p);
-	bool FindParent(TreeNode<T>* t, TreeNode<T>* p);
+	TreeNode<T>* root, * current;			//根节点和当前节点
+	bool Find(TreeNode<T>* p, T val,bool isTrans);	//从p开始找值为val的节点
+	void RemoveSubTree(TreeNode<T>* p);		//移除p的左分支
+	bool FindParent(TreeNode<T>* t, TreeNode<T>* p);//从p开始找到t节点的父节点
+	void PrintAsTree(TreeNode<T>* t, int deep);//前序遍历，输出树结构
 };
 
 template<class T>
 inline Tree<T>::Tree(T ancestor)
 {
-	TreeNode<T>* _root = new TreeNode<T>(ancestor, NULL, NULL);
+	TreeNode<T>* _root = new TreeNode<T>(ancestor, nullptr, nullptr);
 	root = current = _root;
 }
 
 template<class T>
 inline bool Tree<T>::Root()
 {
-	if (root == NULL) {
+	if (root == nullptr) {
 		current = root;
 		return false;
 	}
@@ -56,30 +60,30 @@ inline bool Tree<T>::Root()
 template<class T>
 inline bool Tree<T>::firstChild()
 {
-	if (current != NULL & current->firstChild != NULL) {
+	if (current != nullptr & current->firstChild != nullptr) {
 		current = current->firstChild;
 		return true;
 	}
-	current = NULL;
+	current = nullptr;
 	return false;
 }
 
 template<class T>
 inline bool Tree<T>::nextSibling()
 {
-	if (current != NULL & current->nextSibling != NULL) {
+	if (current != nullptr & current->nextSibling != nullptr) {
 		current = current->nextSibling;
 		return true;
 	}
-	current = NULL;
+	current = nullptr;
 	return false;
 }
 
 template<class T>
 inline bool Tree<T>::parent()
 {
-	if (current == root || current == NULL) {
-		current = NULL;
+	if (current == root || current == nullptr) {
+		current = nullptr;
 		return false;
 	}
 	return FindParent(root, current);
@@ -89,18 +93,18 @@ template<class T>
 inline bool Tree<T>::FindParent(TreeNode<T>* t, TreeNode<T>* p)
 {
 	TreeNode<T>* temp = t->firstChild;
-	while (temp != NULL && temp != p) {
+	while (temp != nullptr && temp != p) {//树的先根次序遍历
 		if (FindParent(temp, p)) {
 			return true;
 		}
 		temp = temp->nextSibling;
 	}
-	if (temp != NULL && temp == p) {
+	if (temp != nullptr && temp == p) {
 		current = t;
 		return true;
 	}
 	else {
-		current = NULL;
+		current = nullptr;
 		return false;
 	}
 }
@@ -108,7 +112,7 @@ inline bool Tree<T>::FindParent(TreeNode<T>* t, TreeNode<T>* p)
 template<class T>
 inline bool Tree<T>::isEmpty()
 {
-	if (root == NULL) {
+	if (root == nullptr) {
 		return true;
 	}
 	return false;
@@ -117,8 +121,11 @@ inline bool Tree<T>::isEmpty()
 template<class T>
 inline bool Tree<T>::changeName(T nameBef, T nameNow)
 {
+	if (find(nameNow, false)) {//防止家庭成员重名，此搜索无需转移当前指针
+		cout << "更改名与现有成员重复\n"; return false;
+	}
 	if (current->data != nameBef) {
-		if (!find(nameBef)) {
+		if (!find(nameBef,true)) {
 			return false;
 		}
 	}
@@ -127,23 +134,30 @@ inline bool Tree<T>::changeName(T nameBef, T nameNow)
 }
 
 template<class T>
-inline bool Tree<T>::find(T val)
+inline bool Tree<T>::find(T val,bool isTrans)
 {
 	if (isEmpty())
 		return false;
-	return Find(root, val);
+	if (Find(root, val,isTrans))
+		return true;
+	else {
+		current=root;//找不大就返回根节点
+		return false;
+	}
 }
 
 template<class T>
-inline bool Tree<T>::Find(TreeNode<T>* p, T val)
+inline bool Tree<T>::Find(TreeNode<T>* p, T val,bool isTrans)
 {
-	bool res = false;
-	if (p->data == val) {
-		current = p; res = true;
+	bool res = false;//标记是否找到
+	if (p->data == val) {//终止条件
+		if (isTrans)//是否转移当前节点到所查询的对象
+			current = p;
+		res = true;
 	}
 	else {
-		TreeNode<T>* temp = p->firstChild;
-		while (temp != NULL && !(res = Find(temp, val))) {
+		TreeNode<T>* temp = p->firstChild;//从第一个子女节点开始
+		while (temp != nullptr && !(res = Find(temp, val,isTrans))) {//遍历其兄弟节点
 			temp = temp->nextSibling;
 		}
 	}
@@ -153,7 +167,7 @@ inline bool Tree<T>::Find(TreeNode<T>* p, T val)
 template<class T>
 inline void Tree<T>::RemoveSubTree(TreeNode<T>* p)
 {
-	if (p != NULL) {
+	if (p != nullptr) {//后序遍历释放指针
 		RemoveSubTree(p->firstChild);
 		RemoveSubTree(p->nextSibling);
 		delete p;
@@ -163,54 +177,65 @@ inline void Tree<T>::RemoveSubTree(TreeNode<T>* p)
 template<class T>
 inline bool Tree<T>::insertChild(T father, T val)
 {
-	if (current->data != father) {
-		if (!find(father)) {
+	if (find(val,false))return false;//家谱成员不重名
+	if (current->data != father) {//找到父节点
+		if (!find(father,true)) {
 			return false;
 		}
 	}
-	if (current->firstChild == NULL) {
-		TreeNode<T>* fc = new TreeNode<T>(val,NULL,NULL);
+	TreeNode<T>* currenttemp = current;
+	if (current->firstChild == nullptr) {//父节点无子女，添加成员为左子女
+		TreeNode<T>* fc = new TreeNode<T>(val, nullptr, nullptr);
 		current->firstChild = fc;
 		return true;
 	}
-	else {
-		return insertSibling(current->firstChild->data, val);
+	else {//否则作为长子的弟弟添加到右兄弟节点
+		if (insertSibling(current->firstChild->data, val)) {
+			current = currenttemp;//insertSibling会转移当前节点到长子，恢复指向父节点
+			return true;
+		}
+		else
+			return false;
 	}
 }
 
 template<class T>
 inline bool Tree<T>::insertSibling(T bother, T val)
 {
-	if (current->firstChild->data != bother) {
-		if (!find(bother)) {
+	if (find(val,false))return false;//家谱成员不重名
+	TreeNode<T>* temp = current->firstChild;
+	if (current->firstChild->data != bother) {//找到兄弟节点
+		if (!find(bother,true)) {
 			return false;
 		}
+		else
+			temp = current;
 	}
-	TreeNode<T>* temp = current->firstChild;
-	while (temp->nextSibling != NULL) {
+	while (temp->nextSibling != nullptr) {//找到最右端的位置
 		temp = temp->nextSibling;
 	}
-	TreeNode<T>* ns = new TreeNode<T>(val, NULL, NULL);
-	temp->nextSibling = ns;
+	TreeNode<T>* ns = new TreeNode<T>(val, nullptr, nullptr);
+	temp->nextSibling = ns;//添加成员到右节点
 	return true;
 }
 
 template<class T>
 inline bool Tree<T>::destroyFamily(T father)
 {
-	if (current->data != father) {
-		if (!find(father)) {
+	if (current->data != father) {//找到父节点
+		if (!find(father,true)) {
 			return false;
 		}
 	}
-	RemoveSubTree(current->firstChild);
+	RemoveSubTree(current->firstChild);//释放所有后代节点
+	current->firstChild = nullptr;//注意父节点左指针清空
 	return true;
 }
 
 template<class T>
 inline bool Tree<T>::printCurrent()
 {
-	if (current != NULL) {
+	if (current != nullptr) {
 		cout << current->data;
 		return true;
 	}
@@ -220,16 +245,86 @@ inline bool Tree<T>::printCurrent()
 template<class T>
 inline void Tree<T>::printCurrentChild()
 {
-	if (current != NULL) {
-		if (current->firstChild == NULL) {
+	if (current != nullptr) {
+		if (current->firstChild == nullptr) {
 			cout << current->data << "还未有子女" << endl;
 		}
-		else {
+		else {//打印当前节点所有子女
 			TreeNode<T>*temp = current->firstChild;
-			while (temp != NULL) {
+			while (temp != nullptr) {
 				cout << temp->data << " ";
 				temp = temp->nextSibling;
 			}
 		}
 	}
+}
+
+template<class T>
+inline void Tree<T>::PrintAsTree(TreeNode<T>* t, int deep)
+{
+	if (t != nullptr) {//前序遍历
+		for (int i = 1; i < deep; i++) {//树状修饰
+			if (i == deep - 1)
+				printf("+-------");
+			else
+				printf("|       ");
+		}
+		cout << t->data << endl;
+		PrintAsTree(t->firstChild, deep + 1);
+		PrintAsTree(t->nextSibling, deep);
+	}
+}
+
+/*******************************************/
+//简易自定义string类
+class mystring {
+public:
+	mystring();//构造函数
+	~mystring();//析构函数
+	mystring(const mystring& str);//拷贝构造函数
+	mystring& operator=(const mystring& str);//等号赋值重载
+	bool operator==(const mystring& str)const { return strcmp(_str, str._str) == 0 ? true : false; }
+	bool operator!=(const mystring& str)const { return strcmp(_str, str._str) != 0 ? true : false; }
+	friend ostream& operator<<(ostream& os, const mystring& str){os << str._str;return os;}
+	friend istream& operator>>(istream& is, const mystring& str){is >> str._str;return is;}
+	int size()const { return _len; }
+private:
+	char* _str;
+	int _len;
+};
+
+mystring::mystring() :_len(0)
+{
+	_str = new char[maxLength];
+	_str[0] = '\0';
+}
+
+mystring::~mystring() {
+	delete _str;
+	_str = NULL;
+}
+
+mystring::mystring(const mystring& str)
+{
+	_str = new char[maxLength + 1];
+	if (!_str)
+	{
+		cerr << "内存分配错误!\n";
+		exit(1);
+	}
+	_len = str._len;
+	strcpy(_str, str._str);
+}
+
+mystring& mystring::operator=(const mystring& str)
+{
+	_str = new char[maxLength + 1];
+	if (!_str)
+	{
+		cerr << "内存分配错误!\n";
+		exit(1);
+	}
+	_len = str._len;
+	strcpy(_str, str._str);
+	return *this;
 }
