4b5ddb8bda0e70b8ee623cfa0b8c8398e31c8a23
afishspring <104251938+afishspring@users.noreply.github.com>
2022-12-11 03:44:31 +0800

    考试系统终稿
    说明文档、修改bug、添加了些许边界条件

--- 考试报名系统/main.cpp
+++ 考试报名系统/main.cpp
@@ -1,13 +1,25 @@
-#include<iostream>
-#include"mylist.hpp"
+#include <iostream>
+#include "mylist.h"
 using namespace std;
 
-void input(list<student>&_l,int num) {
+bool input(list<student>&_l,int num) {
     student temp;
     for (int i = 0; i < num; i++) {
         cin >> temp;
+        if (cin.fail()) {
+            cerr << "输入错误\n";
+            cin.clear();
+            cin.ignore(INT_MAX, '\n');
+            return false;
+        }
+        if (_l.find(temp.no)!=-1) {
+            cerr << temp.no << "号考生已存在\n";
+            i--;
+            continue;
+        }
         _l.push_back(temp);
     }
+    return true;
 }
 
 void insertData(list<student>& _l) {
@@ -15,9 +27,37 @@ void insertData(list<student>& _l) {
     student newStudent;
     cout << "请输入你要插入的考生的位置：";
     cin >> pos;
+    if (cin.fail()||pos>_l.get_length()+1&& _l.get_length()!=0||pos<=0) {
+        cerr << "插入位置只能在"<<1<<"~"<< _l.get_length()+1<<"之间\n";
+        cin.clear();
+        cin.ignore(INT_MAX, '\n');
+        return;
+    }
+    else if (_l.get_length() == 0 && pos != 1) {
+        cerr << "系统数据为空时，插入位置为1\n";
+        cin.clear();
+        cin.ignore(INT_MAX, '\n');
+        return;
+    }
     cout << "请依次输入要插入的考生的考号，姓名，性别，年龄及报考类别!" << endl;
     cin >> newStudent;
-    _l.insert_node(pos-1, newStudent);
+    if (cin.fail()) {
+        cerr << "输入错误\n";
+        cin.clear();
+        cin.ignore(INT_MAX, '\n');
+        return;
+    }
+    if (_l.find(newStudent.no) != -1) {
+        cerr << newStudent.no << "号考生已存在\n";
+        return;
+    }
+    if (_l.get_length() == 0) {
+        _l.push_back(newStudent);
+
+    }
+    else {
+        _l.insert_node(pos - 1, newStudent);
+    }
     _l.print();
 }
 
@@ -32,7 +72,7 @@ void deleteData(list<student>& _l) {
     }
     else {
         cout << "你删除的考生信息是：";
-        cout << _l.del_node(pos);
+        _l.del_node(pos);
         _l.print();
     }
 }
@@ -72,19 +112,32 @@ int main()
     list<student>system;
     int num;
     cout << "首先请建立考生信息系统!" << endl;
-    cout << "请输入考生人数：";
-    cin >> num;
+    while (true) {
+        cout << "请输入考生人数：";
+        cin >> num;
+        if (cin.fail() || num <= 0) {
+            cerr << "考生人数输入错误\n";
+            cin.clear();
+            cin.ignore(INT_MAX, '\n');
+        }
+        else
+            break;
+    }
     cout << "请依次输入考生的考号，姓名，性别，年龄及报考类别!" << endl;
-    input(system,num);
+    while (true) {
+        if (input(system, num))break;
+    }
     system.print();
 
     int oper = 0;
-    cout << "请选择您要进行的操作（1为插入，2为删除，3为查找，4为修改，5为统计，0为取消操作）" << endl << endl;
+    cout << "\n请选择您要进行的操作（1为插入，2为删除，3为查找，4为修改，5为统计，0为取消操作）" << endl;
     while (oper >= 0) {
-        cout << "请选择您要进行的操作：";
+        cout << "\n请选择您要进行的操作：";
         cin >> oper;
         if (cin.fail()) {
-            cerr << "操作数非法，请重新输入!";
+            cerr << "操作数非法，请重新输入!\n";
+            cin.clear();
+            cin.ignore(INT_MAX, '\n');
             continue;
         }
         switch (oper) {
@@ -107,7 +160,7 @@ int main()
             system.print();
             break;
         default:
-            cerr << "操作数错误，请重新输入!" << endl;
+            cerr << "操作数错误，请重新输入!\n";
             break;
         }
     }
--- 考试报名系统/mylist.h
+++ 考试报名系统/mylist.h
@@ -1,16 +1,19 @@
+#pragma once
 #include <iostream>
+#include <cstring>
 #include <iomanip>
+#define maxLength 50
 using namespace std;
+
 //student类
 typedef struct student {
-	int no;
-	string name;
-	string gender;
-	int age;
-	string type;
-	student():no(-1),age(-1) {}
+	int no;						//考号
+	char name[maxLength];		//姓名
+	char gender[maxLength];		//性别
+	int age;					//年龄
+	char type[maxLength];		//报考类型
 	friend istream& operator>>(istream& is, student& rhs) {
-		is >> rhs.no >> rhs.name >> rhs.gender>> rhs.age >> rhs.type;
+		is >> rhs.no>> rhs.name >> rhs.gender>> rhs.age>> rhs.type;
 		return is;
 	}
 	friend ostream& operator<<(ostream& os, const student& rhs) {
@@ -23,6 +26,7 @@ typedef struct student {
 		return os;
 	}
 }student;
+
 //声明
 template<class T>
 class list;
@@ -30,34 +34,34 @@ class list;
 template <class T>
 class node {
 public:
-	node():next(nullptr){}
+	node():next(nullptr) {}
 	node(T val):data(val),next(nullptr){}
 	friend ostream& operator<<(ostream& os, const node& rhs) {
 		os << rhs.data;
 		return os;
 	}
 private:
-	T data;
-	node* next;
+	T data;    //考生信息
+	node* next;//后继指针
 	friend class list<T>;
 };
 //链表类，附有头结点、尾结点
 template <class T>
 class list {
 public:
-	list();
-	list(const list<T>& _list);
-	list<T>& operator= (const list<T>& _list);
+	list();									   //构造函数
+	list(const list<T>& _list);				   //拷贝构造函数
+	~list() { makeEmpty(); }				   //析构函数
 	bool insert_node(int index, T val);        //插入：值为val的节点到序号为index之后
-	node<T>* del_node(int index);              //删除index号节点，范围1~list长度，返回删除的节点
+	bool del_node(int index);				   //删除index号节点，范围1~list长度
 	void modify_node(int index, T val);        //修改index号节点的值为val
 	T get_node(int index);                     //返回index号节点的数据               
 	int find(int value);                       //返回值为value的节点的序号
 	int get_length();                          //返回链表长度
 	void push_back(T val);                     //在链表末尾添加
 	void print();                              //打印链表
-	~list();
 private:
+	void makeEmpty();
 	node<T>* p_head;   //头指针，指向为空的头结点
 	node<T>* p_end;    //末指针，指向尾结点前一个节点
 	int length;
@@ -66,32 +70,30 @@ private:
 template<class T>
 list<T>::list()
 {
-	node<T>* head = new node<T>;
-	node<T>* end = new node<T>;
-	this->p_head = head;
-	this->p_end = head;
-	head->next = end;
-	end->next = nullptr;
+	this->p_head = new node<T>;
+	this->p_end = new node<T>;
+	this->p_head = this->p_end;
+	p_end->next = nullptr;
 	this->length = 0;
 }
 
 template<class T>
 list<T>::list(const list<T>& _list)
 {
-	if (_list.p_head->next->next == nullptr) {    //_list为空时
-		node<T>* head = new node<T>;
-		node<T>* end = new node<T>;
-		this->p_head = head;
-		this->p_end = head;
-		head->next = end;
-		end->next = nullptr;
+	if (_list.length()==0) {    //_list为空时，新建链表
+		this->p_head = new node<T>;
+		this->p_end = new node<T>;
+		this->p_head = this->p_end;
+		p_end->next = nullptr;
 		this->length = 0;
 	}
-	else {               //_list不为空时
+	else {               //_list不为空时，逐一赋值
+		if (_list.length < this->length)makeEmpty();//链表空间过大的指针需要释放
 		this->p_head = new node<T>;
 		node<T>* p_copy = new node<T>;
 		p_head->next = p_copy;
 		for (node<T>* p_ori = _list.p_head->next; p_ori->next != nullptr; p_ori = p_ori->next) {
+			//遍历原始数据链表
 			p_copy.data = p_ori.data;
 			p_copy->next = new node<T>;
 			this->p_end = p_copy;
@@ -103,25 +105,18 @@ list<T>::list(const list<T>& _list)
 }
 
 template<class T>
-list<T>& list<T>::operator=(const list<T>& _list)
-{
-	// TODO: 在此处插入 return 语句
-}
-
-template<class T>
 bool list<T>::insert_node(int index, T val)
 {
 	if (index > this->length || index < 0) {
-		cerr << "index out of range";
+		cerr << "序号超范围\n";
 		return false;
 	}
 	else {
-		node<T>* p_findpos = this->p_head;
-		node<T>* p_new = new node<T>;
+		node<T>* p_findpos = this->p_head;//插入位置的节点
+		node<T>* p_new = new node<T>(val);  //插入的新节点
 		for (int i = 1; i <= index; i++) {
 			p_findpos = p_findpos->next;
-		}
-		p_new->data = val;
+		}//找到插入位置index
 		p_new->next = p_findpos->next;
 		p_findpos->next = p_new;
 		this->length++;
@@ -133,37 +128,39 @@ bool list<T>::insert_node(int index, T val)
 }
 
 template<class T>
-node<T>* list<T>::del_node(int index)
+bool list<T>::del_node(int index)
 {
 	if (index > this->length || index <= 0) {
-		cerr << "index out of range";
-		return nullptr;
+		cerr << "序号超范围\n";
+		return false;
 	}
-	node<T>* p_findpos = this->p_head;
-	node<T>* p_temp = nullptr;
-	node<T>* p_return = nullptr;
+	node<T>* p_findpos = this->p_head;//删除位置的前一节点
+	node<T>* p_return = nullptr;//删除位置节点
 	for (int i = 1; i < index; i++) {
 		p_findpos = p_findpos->next;
 	}
-	p_temp = p_findpos->next->next;
 	p_return = p_findpos->next;
-	delete p_findpos->next;
-	p_findpos->next = p_temp;
+	p_findpos->next = p_return->next;
+	cout << *p_return;//按项目要求输出
 	if (index == this->length) {
 		this->p_end = p_findpos;
-	}
+	}//若删除尾结点，需要更新尾结点
 	this->length--;
-	return p_return;
+	delete p_return;
+	return true;
 }
 
 template<class T>
 void list<T>::modify_node(int index, T val) {
 	if (index > this->length || index <= 0) {
-		cerr << "index out of range";
+		cerr << "序号超范围\n";
 		return;
 	}
-	node<T>* p_findpos = this->p_head;
-	node<T>* p_temp = nullptr;
+	if (find(val.no) != -1) {//查重
+		cerr <<val.no<< "号考生已存在，无法修改"<<index<<"号考生信息\n";
+		return;
+	}
+	node<T>* p_findpos = this->p_head;//修改位置的节点
 	for (int i = 1; i <= index; i++) {
 		p_findpos = p_findpos->next;
 	}
@@ -174,7 +171,7 @@ template<class T>
 T list<T>::get_node(int index)
 {
 	if (index > this->length || index <= 0) {
-		cerr << "index out of range";
+		cerr << "序号超范围\n";
 	}
 	node<T>* p_findpos = this->p_head;
 	for (int i = 1; i <= index; i++) {
@@ -187,6 +184,7 @@ template<class T>
 int list<T>::find(int value)
 {
 	node<T>* p_findpos = this->p_head->next;
+	//从头遍历到尾
 	for (int i = 1; i <= this->length; i++) {
 		if (p_findpos->data.no == value) {
 			return i;
@@ -205,17 +203,34 @@ int list<T>::get_length()
 template<class T>
 void list<T>::push_back(T val)
 {
+	//用尾结点指针做辅助
 	node<T>* p_new = new node<T>;
 	p_new->data = val;
-	p_new->next = this->p_end->next;
+	p_new->next = nullptr;
 	this->p_end->next = p_new;
 	this->p_end = p_new;
 	this->length++;
 }
 
 template<class T>
-list<T>::~list()
+inline void list<T>::print()
 {
+	cout << endl << "考号    姓名    性别    年龄    报考类别" << endl;
+	if (this->p_head == this->p_end) {
+		cout << "系统无数据\n";
+		return;
+	}
+	node<T>* p = this->p_head->next;
+	while (p!= nullptr) {
+		cout << *p;
+		p = p->next;
+	}
+}
+
+template<class T>
+inline void list<T>::makeEmpty()
+{
+	//从头开始逐一释放
 	node<T>* p1 = this->p_head;
 	node<T>* p2 = p1->next;
 	while (p2 != nullptr) {
@@ -226,19 +241,4 @@ list<T>::~list()
 	delete p1;
 	this->length = 0;
 	this->p_head = nullptr;
-}
-
-template<class T>
-inline void list<T>::print()
-{
-	if (this->p_head->next == this->p_end) {
-		cout << "list is empty";
-		return;
-	}
-	cout << endl << "考号    姓名    性别    年龄    报考类别" << endl;
-	node<T>* p = this->p_head->next;
-	while (p->next != nullptr) {
-		cout << *p;
-		p = p->next;
-	}
 }
